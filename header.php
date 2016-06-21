d31a9a3b35db14c73c2172ecc01e4f030b466a8e
Jaspreet Bagga <jbagga93@live.ca>
2016-06-20 20:25:12 -0700

    php integration check
    added php

--- header.php
+++ header.php
@@ -1,14 +1,5 @@
-<!DOCTYPE HTML5>
-<html>
 
-<head>
-    <meta charset="utf-8" />
-    <title>JASS'S JOURNAL OF JABBER</title>
-    <link rel="stylesheet" type="text/css" href="css/index.css">
-</head>
-
-<body>
-    <header>
+<header>
         <h1><a href="index.html">JASS BAGGA</a></h1>
         <ul class="nav-list">
             <li>
@@ -64,40 +55,3 @@
             </li>
         </ul>
     </header>
-    <div class="jumbotron">
-        <h1>Welcome to my portfolio!</h1>
-        
-    </div>
-    <div class="footer">
-        <h1>Contact Me:</h1>
-        <div class="contact-icons">
-            <a href="https://ca.linkedin.com/in/jbagga">
-                <div class="contact">
-                    <img src="img/linkedin.svg"></img>
-                </div>
-            </a>
-            <a href="https://twitter.com/baggajaspreet">
-                <div class="contact">
-                    <img src="img/twitter.svg"></img>
-                </div>
-            </a>
-            <a href="https://github.com/jbagga">
-                <div class="contact">
-                    <img src="img/github.svg"></img>
-                </div>
-            </a>
-            <a href="https://www.facebook.com/jbagga93">
-                <div class="contact">
-                    <img src="img/facebook.svg"></img>
-                </div>
-            </a>
-            <a href="https://instagram.com/jbagga93/">
-                <div class="contact">
-                    <img src="img/instagram.svg"></img>
-                </div>
-            </a>
-        </div>
-    </div>
-</body>
-
-</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML5>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>JASS'S JOURNAL OF JABBER</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+   <?=$header?>
+    <div class="jumbotron">
+        <h1>Welcome to my portfolio!</h1>
+        
+    </div>
+    <div class="footer">
+        <h1>Contact Me:</h1>
+        <div class="contact-icons">
+            <a href="https://ca.linkedin.com/in/jbagga">
+                <div class="contact">
+                    <img src="img/linkedin.svg"></img>
+                </div>
+            </a>
+            <a href="https://twitter.com/baggajaspreet">
+                <div class="contact">
+                    <img src="img/twitter.svg"></img>
+                </div>
+            </a>
+            <a href="https://github.com/jbagga">
+                <div class="contact">
+                    <img src="img/github.svg"></img>
+                </div>
+            </a>
+            <a href="https://www.facebook.com/jbagga93">
+                <div class="contact">
+                    <img src="img/facebook.svg"></img>
+                </div>
+            </a>
+            <a href="https://instagram.com/jbagga93/">
+                <div class="contact">
+                    <img src="img/instagram.svg"></img>
+                </div>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
