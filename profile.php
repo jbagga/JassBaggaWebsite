1c7c5bd5797b8cd6823b5d41133b70f922711a46
Jaspreet Bagga <jbagga93@live.ca>
2016-06-22 14:14:29 -0700

    Changed profile

--- profile.php
+++ profile.php
@@ -11,15 +11,18 @@
 <?php
 include('jassHeader.php');
 ?>
-        <div id="about-me-img">
-            <img src="img/profilephoto.jpg"></img>
+        <div id="about-me-img" class="jumbotron">
+            <img src="img/profileJumbotron.jpg"></img>
         </div>
         <div class="about-me">
             <h1>About Me:</h1>
             <p> My name is Jass. I just finished my Bachelor's degree in Bioinformatics at Simon Fraser University, in British Columbia, Canada.
             </p>
             <p>
-                Since the summer of 2013, I have worked as a Software Engineering intern at Microsoft. I will soon be working full time as an engineer. Until then I am teaching myself more about Operating Systems and creating my website from scratch.
+                Since the summer of 2013, I have worked as a Software Engineering intern at Microsoft. I will soon be working full time as an engineer. 
+            </p>
+            <p>
+            Until then I am teaching myself more about Operating Systems and creating my website from scratch.
             </p>
         </div>
         
